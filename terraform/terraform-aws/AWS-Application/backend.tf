3137dd85957630ac85704248a818460b1e6cd61c
Hortencia <hortencialopesbs@gmail.com>
2025-07-13 11:48:25 -0300

    Adding files to the infra (backend, main)

---
+++ terraform/terraform-aws/AWS-Application/backend.tf
@@ -0,0 +1,13 @@
+/*
+Store the terraform state in s3 bucket and the lock with DynamoDB table
+*/
+
+terraform {
+    backend "s3" {
+      bucket         = "simetrik-terraform-tfstate"
+      key            = "simetrik-terraform-tfstate/aws-application/terraform.tfstate"
+      region         = "sa-east-1"
+      profile        = "terraform-profile"
+      dynamodb_table = "simetrik-terraform-locks"
+    }
+}
