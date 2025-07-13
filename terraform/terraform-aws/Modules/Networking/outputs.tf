11569d3ce2f7bac5e4d9967cc8cb4600d1ec3d1e
Hortencia <hortencialopesbs@gmail.com>
2025-07-13 00:04:22 -0300

    Criando iam.tf e ajustando outputs do modulo de Networking

--- terraform/terraform-aws/Modules/Networking/outputs.tf
+++ terraform/terraform-aws/Modules/Networking/outputs.tf
@@ -30,3 +30,11 @@ output "private_subnet_az1_id" {
 output "private_subnet_az2_id" {
   value = aws_subnet.private_subnet_az2.id
 }
+
+output "nat_gateway_az1" {
+  value = aws_nat_gateway.nat_gateway_az1.id
+}
+
+output "nat_gateway_az2" {
+  value = aws_nat_gateway.nat_gateway_az2.id
+}
